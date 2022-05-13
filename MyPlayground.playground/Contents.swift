f4dd6480c75fac01f5199602ffff4cd4f7e0bc34
Matthias Zarzecki <matthias.zarzecki@chrono24.com>
2022-05-13 13:21:15 +0200

    Add more details, comments

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,13 +3,22 @@ import Combine
 import PlaygroundSupport
 
 // Outputs 1 piece of data every second
+// A publisher alone doesn't do anything, it needs a subscription
 let subscription = Timer.publish(every: 1, on: .main, in: .common)
   .autoconnect()
-  .sink { output in
-    print("finishes steam with \(output)")
-  } receiveValue: { value in
-    print("receive value \(value)")
-  }
+  // SINK: Attaches a subscriber with closure-based behavior
+  // to a publisher that never fails.
+  .sink(
+    receiveCompletion: { output in
+      // Called once, when the publisher was completed.
+      print("finishes steam with \(output)")
+    },
+    receiveValue: { value in
+      // Can be called multiple times, each time that a
+      // new value was emitted by the publisher.
+      print("receive value \(value)")
+    }
+  )
 
 // Stops the publisher after 5 seconds
 RunLoop.main.schedule(after: .init(Date(timeIntervalSinceNow: 5))) {
